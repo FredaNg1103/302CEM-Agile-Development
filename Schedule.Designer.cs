8cb1b9867adba31a9ce96f31fc756b061ad7ca7f
Derrick <derrick94kong@gmail.com>
2018-02-23 10:35:56 +0800

    entire changes

--- Schedule.Designer.cs
+++ Schedule.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WindowsFormsApp1
+namespace CnL_Scheduling
 {
-    partial class Schedule
+    partial class Scheduling
     {
         /// <summary>
         /// Required designer variable.
@@ -28,120 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Scheduling));
+            this.WelcomeMessage = new System.Windows.Forms.Label();
+            this.LogoutBtn = new System.Windows.Forms.Button();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CnC_List = new System.Windows.Forms.ComboBox();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.Classroom_rdb = new System.Windows.Forms.RadioButton();
+            this.Course_rdb = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // WelcomeMessage
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(44, 29);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(548, 245);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.WelcomeMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeMessage.Location = new System.Drawing.Point(704, 12);
+            this.WelcomeMessage.Name = "WelcomeMessage";
+            this.WelcomeMessage.Size = new System.Drawing.Size(390, 25);
+            this.WelcomeMessage.TabIndex = 3;
+            this.WelcomeMessage.Text = "Welcome, User";
+            this.WelcomeMessage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // button1
+            // LogoutBtn
             // 
-            this.button1.Location = new System.Drawing.Point(391, 435);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 35);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Confirm";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.LogoutBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoutBtn.Image = ((System.Drawing.Image)(resources.GetObject("LogoutBtn.Image")));
+            this.LogoutBtn.Location = new System.Drawing.Point(1100, 12);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(70, 70);
+            this.LogoutBtn.TabIndex = 2;
+            this.LogoutBtn.UseVisualStyleBackColor = true;
+            this.LogoutBtn.Click += new System.EventHandler(this.LogoutBtn_Click);
             // 
-            // button2
+            // BackBtn
             // 
-            this.button2.Location = new System.Drawing.Point(506, 435);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 35);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.BackBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackBtn.Location = new System.Drawing.Point(1057, 699);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(113, 42);
+            this.BackBtn.TabIndex = 17;
+            this.BackBtn.Text = "Back";
+            this.BackBtn.UseVisualStyleBackColor = true;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
             // 
-            // label1
+            // DateLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 294);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateLabel.Location = new System.Drawing.Point(50, 100);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(81, 29);
+            this.DateLabel.TabIndex = 18;
+            this.DateLabel.Text = "Date : ";
             // 
-            // label2
+            // dateTimePicker
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 340);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker.Location = new System.Drawing.Point(137, 100);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(228, 30);
+            this.dateTimePicker.TabIndex = 19;
+            this.dateTimePicker.Value = new System.DateTime(2018, 2, 22, 2, 8, 51, 0);
             // 
-            // label3
+            // CnC_List
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 385);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.CnC_List.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CnC_List.FormattingEnabled = true;
+            this.CnC_List.Location = new System.Drawing.Point(55, 226);
+            this.CnC_List.Name = "CnC_List";
+            this.CnC_List.Size = new System.Drawing.Size(310, 37);
+            this.CnC_List.TabIndex = 21;
             // 
-            // label4
+            // CheckBtn
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(294, 294);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
+            this.CheckBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBtn.Location = new System.Drawing.Point(406, 222);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(113, 42);
+            this.CheckBtn.TabIndex = 22;
+            this.CheckBtn.Text = "Check";
+            this.CheckBtn.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // Classroom_rdb
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(294, 340);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "label5";
+            this.Classroom_rdb.AutoSize = true;
+            this.Classroom_rdb.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Classroom_rdb.Location = new System.Drawing.Point(55, 174);
+            this.Classroom_rdb.Name = "Classroom_rdb";
+            this.Classroom_rdb.Size = new System.Drawing.Size(150, 33);
+            this.Classroom_rdb.TabIndex = 0;
+            this.Classroom_rdb.TabStop = true;
+            this.Classroom_rdb.Text = "Classroom";
+            this.Classroom_rdb.UseVisualStyleBackColor = true;
+            this.Classroom_rdb.CheckedChanged += new System.EventHandler(this.Classroom_rdb_CheckedChanged);
             // 
-            // label6
+            // Course_rdb
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(297, 385);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "label6";
+            this.Course_rdb.AutoSize = true;
+            this.Course_rdb.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Course_rdb.Location = new System.Drawing.Point(211, 174);
+            this.Course_rdb.Name = "Course_rdb";
+            this.Course_rdb.Size = new System.Drawing.Size(112, 33);
+            this.Course_rdb.TabIndex = 1;
+            this.Course_rdb.TabStop = true;
+            this.Course_rdb.Text = "Course";
+            this.Course_rdb.UseVisualStyleBackColor = true;
+            this.Course_rdb.CheckedChanged += new System.EventHandler(this.Course_rdb_CheckedChanged);
             // 
-            // Schedule
+            // Scheduling
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 491);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "Schedule";
-            this.Text = "Schedule";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.Course_rdb);
+            this.Controls.Add(this.CheckBtn);
+            this.Controls.Add(this.Classroom_rdb);
+            this.Controls.Add(this.CnC_List);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.WelcomeMessage);
+            this.Controls.Add(this.LogoutBtn);
+            this.Name = "Scheduling";
+            this.Text = "Scheduling";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,14 +160,14 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label WelcomeMessage;
+        private System.Windows.Forms.Button LogoutBtn;
+        private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox CnC_List;
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.RadioButton Classroom_rdb;
+        private System.Windows.Forms.RadioButton Course_rdb;
     }
 }
